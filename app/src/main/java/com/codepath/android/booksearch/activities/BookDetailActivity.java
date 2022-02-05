a04508a05cca78bbd886aef348b67c25257b01a8
nekozye <nekozeye@gmail.com>
2022-02-04 21:54:13 -0500

    added a simple shareintent system

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -1,12 +1,21 @@
 package com.codepath.android.booksearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
+import androidx.core.view.MenuItemCompat;
 
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,11 +24,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.models.Book;
 
 import org.parceler.Parcels;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class BookDetailActivity extends AppCompatActivity {
     private ImageView ivBookCover;
@@ -27,15 +44,13 @@ public class BookDetailActivity extends AppCompatActivity {
     private TextView tvAuthor;
 
     private Book book;
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
-
-
-
-
 
 
         // Fetch views
@@ -53,7 +68,19 @@ public class BookDetailActivity extends AppCompatActivity {
 
 
         // Load book object from intent extras
-        Glide.with(this).load(Uri.parse(book.getCoverUrl())).apply(new RequestOptions().placeholder(R.drawable.ic_nocover)).into(ivBookCover);
+        Glide.with(this).load(Uri.parse(book.getCoverUrl())).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                prepareShareIntent(((BitmapDrawable) resource).getBitmap());
+                attachShareIntentAction();
+                return false;
+            }
+        }).apply(new RequestOptions().placeholder(R.drawable.ic_nocover)).into(ivBookCover);
         tvTitle.setText(book.getTitle());
         tvAuthor.setText(book.getAuthor());
 
@@ -75,6 +102,42 @@ public class BookDetailActivity extends AppCompatActivity {
     }
 
 
+    public Uri getBitmapFromDrawable(Bitmap bmp){
+        Uri bmpUri = null;
+
+        try {
+            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+
+            bmpUri = FileProvider.getUriForFile(BookDetailActivity.this, "com.codepath.fileprovider", file);
+
+        }
+        catch(IOException e){
+            Log.e("BitmapLogger",e.getMessage());
+        }
+
+        return bmpUri;
+    }
+
+
+    public void prepareShareIntent(Bitmap drawableImage){
+        Uri bmpUri = getBitmapFromDrawable(drawableImage);
+
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+    }
+
+    public void attachShareIntentAction(){
+        if (miShareAction != null && shareIntent != null)
+            miShareAction.setShareIntent(shareIntent);
+    }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -83,6 +146,13 @@ public class BookDetailActivity extends AppCompatActivity {
         // Add Share Intent
         // see http://guides.codepath.org/android/Sharing-Content-with-Intents#shareactionprovider
         // (Bonus) Share book title and cover image using the same intent.
+
+        // Locate Menu Item with SAP
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetching reference
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        attachShareIntentAction();
+        // true for displaying menu
         return true;
     }
 
@@ -94,7 +164,7 @@ public class BookDetailActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_item_share) {
             return true;
         }
 
--- app/src/main/java/com/codepath/android/booksearch/activities/BookListActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookListActivity.java
@@ -177,7 +177,7 @@ public class BookListActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_item_share) {
             return true;
         }
 
